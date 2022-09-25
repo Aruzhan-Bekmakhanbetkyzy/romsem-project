904dbcdae998db53bd32504e394f75a593ae02b0
Aruzhan-Bekmakhanbetkyzy <aruzhanbekmakhanbetkyzy@gmail.com>
2022-09-26 01:34:19 +0600

    styles

--- src/components/Center.js
+++ src/components/Center.js
@@ -1,11 +1,68 @@
-import React from 'react'
+import React from "react";
+import {
+    time,
+    search,
+    food1,
+    food2,
+    food3,
+    food4,
+    food5,
+    food6,
+    food7,
+    food8,
+    food9,
+} from "../images/images.js";
 
 const Center = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    return (
+        <div className="general_center">
+            <div className="top">
+                <div className="top_left_side">
+                    <h4 className="top_left_side_h4">Our phone</h4>
+                    <h3 className="top_left_side_h3">
+                        +702 882 7177 +778 890 7886
+                    </h3>
+                    <img className="time" src={time} alt="time" />
+                    <h5 className="top_left_side_h5">
+                        we work from 10:00 to 00:00
+                    </h5>
+                </div>
+                <p className="top_right_side_p">City:</p>
+                <h4 className="top_right_side_h4">Astana</h4>
+                <div>
+                    <h3 className="reviews">Reviews</h3>
+                    <h3 className="ship_and_pay">Shipping and payment</h3>
+                    <img className="search" src={search} alt="search" />
+                </div>
+            </div>
+            <div className="middle">
+                <div className="image-container">
+                    <img
+                        className="food_1"
+                        src={food1}
+                        alt="philadelphia and salmon"
+                    />
+                    <img className="food_2" src={food2} alt="chicken" />
+                    <img className="food_3" src={food3} alt="pizza" />
+                    <img className="food_4" src={food4} alt="with eel" />
+                    <img className="food_5" src={food5} alt="corn-dog" />
+                    <img className="food_6" src={food6} alt="promotions" />
+                </div>
+                <h2 className="novelties">Novelties</h2>
+                <h2 className="popular">Popular</h2>
+                <div className="food_cards">
+                <div className="salamon_set">
+                    <img src={food7} alt="salamon set" />
+                    <h2>Salamon set</h2>
+                    <h3>1050 grams, 30 pieces</h3>
+                    <div className="food_card_underline"></div>
+                    <h2 className="price">9000 tenge</h2>
+                    <button>I want</button>
+                </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Center
+export default Center;
--- src/components/LeftBar.js
+++ src/components/LeftBar.js
@@ -15,23 +15,31 @@ import {
 
 const LeftBar = () => {
     return (
-        <div>
+        <div className="general_leftbar">
             <div>
                 <img className="logo" src={logo} alt="logo" />
                 <h1>Romsem</h1>
             </div>
             <div className="text">
-              <img src={rightpizza} alt="pizza" />
+              <img className="r_pizza" src={rightpizza} alt="pizza" />
                 <text className="t_1">Pizza</text>
-                <img src={rightsets} alt="" />
+                <img className="r_sets" src={rightsets} alt="sets" />
                 <text className="t_2">Sets</text>
+                <img className="r_wok" src={rightwok} alt="wok" />
                 <text className="t_3">WOK</text>
+                <img className="r_rolls" src={rightrolls} alt="rolls" />
                 <text className="t_4">Rolls</text>
+                <img className="r_sushi" src={rightsushi} alt="sushi" />
                 <text className="t_5">Sushi</text>
+                <img className="r_salads" src={rightsalads} alt="salads" />
                 <text className="t_6">Salads</text>
+                <img className="r_soups" src={rightsoups} alt="soups" />
                 <text className="t_7">Soups</text>
+                <img className="r_corndogs" src={rightcorndogs} alt="corndogs" />
                 <text className="t_8">Corn dogs</text>
+                <img className="r_drinks" src={rightdrinks} alt="drinks" />
                 <text className="t_9">Drinks</text>
+                <img className="r_promotions" src={rightpromotions} alt="promotions" />
                 <text className="t_10">Promotions</text>
             </div>
 
